b7744daf461edda8a6e8c432c2566e59476804f7
Fafnyer <maxcstone1@gmail.com>
2023-09-28 14:27:41 -0500

    completed node

--- Queue/LinkedQueue/Node.java
+++ Queue/LinkedQueue/Node.java
@@ -1,5 +1,28 @@
 package Queue.LinkedQueue;
 
 public class Node<T> {
+     
+    private T info;
+    private Node<T> link;
 
+    public Node(T info, Node<T> link) {
+        this.info = info;
+        this.link = link;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setLink(Node<T> link) {
+        this.link = link;
+    }
+
+    public Node<T> getLink() {
+        return link;
+    }
 }
