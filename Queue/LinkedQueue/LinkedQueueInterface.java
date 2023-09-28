c83c27535b5e5c956cb5942e7d7d3f7b499fa0da
Fafnyer <maxcstone1@gmail.com>
2023-09-28 15:29:06 -0500

    completed interface

--- Queue/LinkedQueue/LinkedQueueInterface.java
+++ Queue/LinkedQueue/LinkedQueueInterface.java
@@ -1,5 +1,13 @@
 package Queue.LinkedQueue;
 
-public interface LinkedQueueInterface {
-    
+public interface LinkedQueueInterface <T> {
+    public void enqueue(Node<T> element);
+
+    public Node dequeue();
+
+    public boolean isEmpty();
+
+    public boolean isFull();
+
+    public int size();
 }
